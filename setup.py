6673e09dba2fa59cdd790cb5ce8a7344b57d9393
Lars Flöer <git@lfloeer.de>
2015-04-05 12:20:45 +0200

    Change setup to reflect new class structure

--- setup.py
+++ setup.py
@@ -3,17 +3,25 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
-ext_module = Extension(
-    "lineprofile.profile",
-    ["lineprofile/profile.pyx"],
+model_module = Extension(
+    "lineprofile.model",
+    ["lineprofile/model.pyx"],
     extra_compile_args=['-O3'],
     extra_link_args=['-lfftw3'],
-    include_dirs = [numpy.get_include()]
+    include_dirs=[numpy.get_include()]
+)
+
+fitter_module = Extension(
+    "lineprofile.fitter",
+    ["lineprofile/fitter.pyx"],
+    extra_compile_args=['-O3'],
+    extra_link_args=['-lfftw3'],
+    include_dirs=[numpy.get_include()]
 )
 
 setup(
-    name = 'lineprofile',
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [ext_module],
+    name='lineprofile',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[model_module, fitter_module],
     packages=['lineprofile']
 )
