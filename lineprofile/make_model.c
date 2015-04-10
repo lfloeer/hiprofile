8d68f3f10e0a194a84a95349c525c2e1e10ea280
Lars Flöer <git@lfloeer.de>
2015-04-10 14:47:02 +0200

    Numeric optimizations

--- lineprofile/make_model.c
+++ lineprofile/make_model.c
@@ -1,14 +1,15 @@
 #include "make_model.h"
 
 void make_model(double complex *fft_input, int fft_size,
-                double *parameters, int n_profiles,
+                double const *parameters, int n_profiles,
                 double d_tau, double v_chan, double v_low) {
 
     int offset;
-    double phi, j0tau, j1tau, tau, j_tau, e;
-    double fint, vsys, vrot, vturb, fsolid, asym, tmp2;
+    double j0tau, j1tau, tau, j_tau, e;
+    double fint, vsys, vrot, vturbrot, fsolid, asym, tmp2;
+    double two_fsolid, fdiff;
     double b_real, b_imag;
-    double complex tmp;
+    double complex tmp, phi;
 
     int n_values = fft_size / 2 + 1;
 
@@ -23,17 +24,18 @@ void make_model(double complex *fft_input, int fft_size,
         fint = pow(10.0, parameters[offset + 0]);
         vsys = parameters[offset + 1];
         vrot = parameters[offset + 2];
-        vturb = parameters[offset + 3];
+        vturbrot = parameters[offset + 3] / vrot;
         fsolid = parameters[offset + 4];
         asym = parameters[offset + 5];
-
-        phi = 2. * (vsys - v_low) / vrot;
+        
+        two_fsolid = 2. * fsolid;
+        fdiff = 1 - fsolid;
+        phi = _Complex_I * 2. * (vsys - v_low) / vrot;
 
         fft_input[0] += fint / v_chan;
 
         for (int i = 1; i < n_values; ++i)
         {
-
             tau = d_tau * vrot * i * -1.0;
             j0tau = j0(tau);
             j1tau = j1(tau);
@@ -41,14 +43,14 @@ void make_model(double complex *fft_input, int fft_size,
             j_tau = j1tau / tau;
             e = 1. / tau * (2. / tau * j1tau - j0tau);
 
-            tmp = fint / v_chan * cexp(_Complex_I * phi * tau);
+            tmp = fint / v_chan * cexp(phi * tau);
 
-            b_real = (1 - fsolid) * j0tau + 2. * fsolid * j_tau;
-            b_imag = asym * ((1 - fsolid) * j1tau + 2. * fsolid * e);
+            b_real = fdiff * j0tau + two_fsolid * j_tau;
+            b_imag = asym * (fdiff * j1tau + two_fsolid * e);
 
             tmp *= b_real + _Complex_I * b_imag;
 
-            tmp2 = (vturb / vrot * tau);
+            tmp2 = vturbrot * tau;
             tmp2 *= tmp2;
 
             tmp *= exp(-2. * tmp2);
