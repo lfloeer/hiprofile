b404c0f2e5fe3e696d116fe23db6100099b712e6
Lars Flöer <git@lfloeer.de>
2015-05-19 16:29:09 +0200

    Docfix

--- lineprofile/utils.py
+++ lineprofile/utils.py
@@ -1,10 +1,10 @@
 import numpy as np
 import itertools as it
 
-def sample_prior(n_sampler, fitter):
 
+def sample_prior(n_sampler, fitter):
     def sample_components():
-        "Get samples from prior on line profile"
+        """Get samples from prior on line profile"""
         for component_idx in range(fitter.n_disks):
             yield np.random.uniform(fitter.fint_min, fitter.fint_max, n_sampler)
             yield np.random.normal(fitter.v_center_mean[component_idx],
@@ -20,7 +20,7 @@ def sample_prior(n_sampler, fitter):
             yield 2 * np.random.beta(fitter.asym_p, fitter.asym_q, n_sampler) - 1.0
 
     def sample_gaussians():
-        "Get samples from prior on gaussians"
+        """Get samples from prior on gaussians"""
         for component_idx in range(fitter.n_disks, fitter.n_disks + fitter.n_gaussians):
             yield np.random.uniform(fitter.fint_min, fitter.fint_max, n_sampler)
             yield np.random.normal(fitter.v_center_mean[component_idx],
@@ -29,12 +29,12 @@ def sample_prior(n_sampler, fitter):
             yield np.random.uniform(fitter.gauss_disp_min, fitter.gauss_disp_max, n_sampler)
 
     def sample_baseline():
-        "Get samples from prior on baseline"
+        """Get samples from prior on baseline"""
         for _ in range(fitter.n_baseline):
             yield np.random.normal(0, 0.1, n_sampler)
 
     def sample_likelihood():
-        "Get samples from prior on posterior parameters"
+        """Get samples from prior on posterior parameters"""
         yield np.random.uniform(fitter.fraction_min, fitter.fraction_max, n_sampler)
         yield np.random.uniform(fitter.std_in_min, fitter.std_in_max, n_sampler)
         yield np.random.normal(0., fitter.mu_out_std, n_sampler)
@@ -43,8 +43,9 @@ def sample_prior(n_sampler, fitter):
     prior_it = it.chain(sample_components(), sample_gaussians(), sample_baseline(), sample_likelihood())
     return np.array([samples for samples in prior_it]).T.copy()
 
+
 def resample_position(position, n_walkers, n_dim, fitter, ball_size=1e-2):
-    "Use rejection sampling to resample the walker positions"
+    """Use rejection sampling to resample the walker positions"""
     scale_factors = np.ones(n_dim)
     scale_factors[3:6 * fitter.n_disks:6] = 10
     scale_factors[2:6 * fitter.n_disks:6] = 100
